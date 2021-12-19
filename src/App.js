bab443acbe7d76ea7c3dbf2539af13cedda2d421
Kishan Kumar <endokishan@Kishans-MacBook-Air.local>
2021-12-20 01:31:20 +0530

    cart feature done

--- src/App.js
+++ src/App.js
@@ -4,18 +4,34 @@ import Navigation from './components/Navigation';
 import ProductsPage from './pages/Products';
 import Product from './pages/Product';
 import Cart from './pages/Cart';
+import {CartContext} from './CartContext';
+import {useState, useEffect} from 'react';
+import {getCart, storeCart} from './helper';
 
 const App = () => {
+    const [cart, setCart] = useState({});
+    useEffect(() => {
+        getCart().then(cart => {
+            setCart(JSON.parse(cart));
+        });
+    }, []);
+
+    useEffect(() => {
+        storeCart(JSON.stringify(cart));
+    }, [cart]);
+
     return (
         <>
             <Router>
-                <Navigation/>
-                <Switch>
-                    <Route path="/" component={Home} exact></Route>
-                    <Route path="/products" exact component={ProductsPage}></Route>
-                    <Route path="/product/:_id" component={Product}></Route>
-                    <Route path="/cart" component={Cart}></Route>
-                </Switch>
+                <CartContext.Provider value={{cart, setCart}}>
+                    <Navigation/>
+                    <Switch>
+                        <Route path="/" component={Home} exact></Route>
+                        <Route path="/products" exact component={ProductsPage}></Route>
+                        <Route path="/product/:_id" component={Product}></Route>
+                        <Route path="/cart" component={Cart}></Route>
+                    </Switch>
+                </CartContext.Provider>
             </Router>
         </>
     )
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,42 @@
 import { Link } from "react-router-dom";
+import {useContext, useState} from 'react';
+import {CartContext} from '../CartContext';
 
 const Product = (prop) => {
+    const [isAdding, setIsAdding] = useState(false);
+    const {cart, setCart} = useContext(CartContext);
+    const addToCart = (event, product) => {
+        event.preventDefault();
+        let _cart = {...cart};
+        if(!_cart.items) {
+            _cart.items = {};
+        }
+        if(_cart.items[product._id]) {
+            _cart.items[product._id] += 1;
+        } else {
+            _cart.items[product._id] = 1
+        }
+        if(!_cart.totalItems) {
+            _cart.totalItems = 0;
+        }
+        _cart.totalItems += 1;
+        setCart(_cart);
+
+        setIsAdding(true);
+        setTimeout(() => {
+            setIsAdding(false)
+        },1000)
+
+        // const cart = {
+        //     items: {
+        //         "60c67bc0f5ee510015f3dda7": 2,
+        //         "60c67b95f5ee510015f3dda5": 3
+        //     },
+        //     totalItems: 5
+        // }
+    }
     const {product} = prop;
+    
     return (
         <Link to={`/product/${product._id}`}>
             <div>
@@ -12,7 +47,7 @@ const Product = (prop) => {
                 </div>
                 <div className="flex justify-between items-center mt-4">
                     <span>₹ {product.price}</span>
-                    <button className="bg-yellow-500 py-1 px-4 rounded-full font-bold">ADD</button>
+                    <button disabled={isAdding} onClick={(e) => {addToCart(e,product)}} className={`${isAdding ? "bg-green-500" : "bg-yellow-500"} py-1 px-4 rounded-full font-bold`}>ADD{isAdding ? "ED" : ""}</button>
                 </div>
             </div>
         </Link>
